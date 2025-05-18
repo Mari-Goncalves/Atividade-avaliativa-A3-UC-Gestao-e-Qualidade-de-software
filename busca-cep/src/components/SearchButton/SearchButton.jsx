f9aed38adff19bf99ac7527b57e6d15f2b81534b
Mariana Goncalves <mari.goncalves004@gmail.com>
2025-05-18 17:31:33 -0300

    Ajuste de formatação

--- busca-cep/src/components/SearchButton/SearchButton.jsx
+++ busca-cep/src/components/SearchButton/SearchButton.jsx
@@ -1,7 +1,11 @@
 import "./style.css";
 
 function SearchButton({ buttonText, onClick }) {
-  return <button className="search_button" onClick={onClick}>{buttonText}</button>;
+  return (
+    <button className="search_button" onClick={onClick}>
+      {buttonText}
+    </button>
+  );
 }
 
 export default SearchButton;
